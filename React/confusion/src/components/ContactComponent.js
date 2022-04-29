4bff44920c9556a6e0e62aac9a0e40ad0ded2c18
aurelpere <aurel.pere@gmail.com>
2022-04-29 13:37:19 +0200

    fetch handling errors

--- React/confusion/src/components/ContactComponent.js
+++ React/confusion/src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
-import {Row, Breadcrumb,BreadcrumbItem,Form,Button,Label,Input,Col} from "reactstrap";
+import {Row, Breadcrumb,BreadcrumbItem,Button,Label,Input,Col} from "reactstrap";
 import {Link} from "react-router-dom";
-import {Control,LocalForm,Errors} from 'react-redux-form';
+import {Control,Form,Errors} from 'react-redux-form';
 
 
 const required=(val)=>val && val.length;
@@ -17,8 +17,9 @@ class Contact extends Component {
     }
 
     handleSubmit(values){
-        console.log("Current state is "+JSON.stringify(values))
-        alert("Current state is "+JSON.stringify(values))
+        console.log("Current state is "+JSON.stringify(values));
+        alert("Current state is "+JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
     validate(firstname,lastname,telnum,email){
@@ -93,7 +94,7 @@ class Contact extends Component {
                     <h3>Send us your feedback</h3>
                 </div>
                 <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values)=>this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values)=>this.handleSubmit(values)}>
                         <Row className="form-group">
                             <Label htmlFor="firstname" md={2}>First Name</Label>
                             <Col md={10}>
@@ -199,7 +200,7 @@ class Contact extends Component {
                             </Col>
                         </Row>
 
-                    </LocalForm>
+                    </Form>
                 </div>
             </div>
         </div>
--- React/confusion/src/components/MainComponent.js
+++ React/confusion/src/components/MainComponent.js
@@ -10,7 +10,8 @@ import About from './AboutComponent'
 import {Routes,Route,Navigate} from 'react-router-dom';
 import Header from './HeaderComponent'
 import {connect} from 'react-redux';
-import {addComment,fetchDishes} from "../redux/ActionCreators";
+import {addComment, fetchComments, fetchDishes, fetchPromos} from "../redux/ActionCreators";
+import {actions} from 'react-redux-form';
 
 import {
   useLocation,
@@ -27,7 +28,10 @@ const mapStateToProps=state=>{
     }
 const mapDispatchToProps=(dispatch)=>({
   addComment:(dishId,rating,author,comment)=>dispatch(addComment(dishId,rating,author,comment)),
-    fetchDishes:()=>{dispatch(fetchDishes())}
+    fetchDishes:()=>{dispatch(fetchDishes())},
+    resetFeedbackForm: ()=>{dispatch(actions.reset('feedback'))},
+    fetchComments:()=>{dispatch(fetchComments())},
+    fetchPromos:()=>{dispatch(fetchPromos())},
 })
 
 
@@ -54,6 +58,8 @@ class Main extends Component {
 
     componentDidMount() {
       this.props.fetchDishes();
+      this.props.fetchComments();
+      this.props.fetchPromos();
     }
 
     render(){
@@ -64,7 +70,9 @@ class Main extends Component {
         <Home dish={this.props.dishes.dishes.filter((dish)=>dish.featured)[0]}
               dishesLoading={this.props.dishes.isLoading}
               dishesErrMess={this.props.dishes.errMess}
-              promotion={this.props.promotions.filter((promotion)=>promotion.featured)[0]}
+              promotion={this.props.promotions.promotions.filter((promotion)=>promotion.featured)[0]}
+              promosLoading={this.props.promotions.isLoading}
+              promosErrMess={this.props.promotions.errMess}
               leader={this.props.leaders.filter((leader)=>leader.featured)[0]}
         />
     );
@@ -76,7 +84,8 @@ class Main extends Component {
           <Dishdetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(dishId, 10))[0]}
                       isLoading={this.props.dishes.isLoading}
                       ErrMess={this.props.dishes.errMess}
-                      comments={this.props.comments.filter((comment) => comment.dishId === parseInt(dishId, 10))}
+                      comments={this.props.comments.comments.filter((comment) => comment.dishId === parseInt(dishId, 10))}
+                      commentsErrMess={this.props.comments.errMess}
                         addComment={this.props.addComment}/>
 
       );
@@ -89,7 +98,7 @@ class Main extends Component {
         <Route exact path='/menu' element={<Menu dishes={this.props.dishes}/> }/>
         <Route path="/menu/:dishId" element={<DishWithId/>}/>
         <Route path='/home' element={<HomePage/>}/>
-        <Route exact path='/contactus' element={<Contact/>}/>
+        <Route exact path='/contactus' element={<Contact resetFeedbackForm={this.props.resetFeedbackForm }/>}/>
         <Route exact path='/aboutus' element={<About leaders={this.props.leaders}/>}/>
 
         <Route path='/' element={
